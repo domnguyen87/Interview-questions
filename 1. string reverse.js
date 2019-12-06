9466f53eb0d0ba4b6b24560c8ac72a84ab1b1ebf
Dominique Nguyen <domnguyen@gmail.com>
2019-12-06 14:32:27 -0800

    Add string reversal

---
+++ 1. string reverse.js
@@ -0,0 +1,36 @@
+//Given a string, return new string with reversed order
+//example 1
+function reverse(str) {
+    return str
+        .split('')
+        .reverse()
+        .join('')
+}
+
+module.exports = reverse
+
+//example 2
+// create an empty string called 'reverse'
+// for each character in the provided string  
+// Take the character and add it to the start of 'reversed'
+
+// Return the Variable 'reverse'
+function reverse(str) {
+    let reversed = '';
+
+    // For Loop classic example.
+    // for (var i = 0; i < str.length; i++) {
+    // }
+
+    //For of syntax
+    for (let character of str) {
+        reversed = character + reversed;
+    }
+
+    return reversed;
+}
+
+//example3
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
