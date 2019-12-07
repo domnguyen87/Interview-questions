9b570a9b5ac1ba121ba7daec97d28fa685409913
domnguyen87 <domnguyen@gmail.com>
2019-12-07 05:20:22 -0800

    added palindrome and reverse int

---
+++ 2. palindrome.js
@@ -0,0 +1,28 @@
+//Given a string, return true if the string is a palindrome
+//or false if it's not. Palindromes are the same words revered
+//Examples
+//palindrome("abba") === true
+//palindrome("asdf") === false
+//make sure you're returning true or false
+
+//Solution 1
+function palindrome(str) {
+    const reversed = str   
+        .split('')
+        .reversed()
+        .join('')
+
+    return str === reversed
+}
+
+//Solution 2
+//Use 'every' to comepare which only works with arrays
+//so you have to turn str to an array first
+function palindrome(str) {
+    return str.split('').every((char,i) => {
+        return char === str[str.length - i -1];
+    });
+}
+
+
+module.exports = palindrome;
