c456792a6ac7720c085c0ae3ae9f68a12b303211
Dominique Nguyen <domnguyen@gmail.com>
2019-12-09 08:43:51 -0800

    updates

--- 10. pyramids.js
+++ 10. pyramids.js
@@ -43,4 +43,22 @@ function pyramid(n) {
 }
 
 //Solution 2 ***Recursion
+function pyramid(n, row = 0, level = "") {
+    //basecase
+    if (row === n) {
+        return;
+    }
 
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    let add;
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#'
+    } else {
+        add = ' ';
+    }
+}
