cdc0ecec69412651218c3cf76586342a1f89d7da
domnguyen87 <domnguyen@gmail.com>
2019-12-09 20:16:05 -0800

    added recursive

--- 9. steps.js
+++ 9. steps.js
@@ -32,10 +32,45 @@ function steps(n){
             }
         }
 
-        console.log(stair)
+    console.log(stair)
     }
 }
 
 //***Recursive solution*/
 //HINT
-//
+//Figure out the bare miniuum pieces of information to represent your problem
+//Give reasonable defaults to the bare minium pieces of info
+//Check the base case. Is there any work left to do? If not, return
+//Do some work. Call your fx again, making sure the arugmens have changed in some fashion
+
+function printNumber(n){
+    //basecase
+    if(n===0){
+        return;
+    }
+console.log(n);
+printNumber(n-1);
+}
+printNumber(10);
+
+//If(row === n), then we have hit the end of our problem
+//If the "stairs" string has a length === n then we are at the end of a row
+//If the length of the stair string is less than or equal to the 
+//row number we're working on, we add a "#", otherwise add a space
+
+function steps(n, row = 0, stair =""){
+    if( n===row ) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+}
