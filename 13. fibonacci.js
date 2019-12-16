c82df2b7b271f86207f9bc84a50b656831192442
Dominique Nguyen <domnguyen@gmail.com>
2019-12-16 10:14:11 -0800

    Add Iterative solution

---
+++ 13. fibonacci.js
@@ -0,0 +1,23 @@
+//Print out the n-th entry in the fibonacci series
+//The fibonacci series is an ordering of numbers where
+//each number is the sum of the preceeding two
+//
+//Example: [0,1,1,2,3,5,8,13,21,34]
+//fib(4)=== 3
+//
+
+//Iterative Solution
+function fib(n) {
+    const result = [0, 1]
+
+    for (let i = 2; i <= n; i++) {
+        const a = results[i - 1]
+        const b = results[i - 2]
+
+        results.push(a + b);
+    }
+
+    return result[n]
+}
+
+//Recursive Solution
