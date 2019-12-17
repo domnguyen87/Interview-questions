8dff324ae600e133f64e522edf8eb876e1b0f4aa
Dominique Nguyen <domnguyen@gmail.com>
2019-12-17 10:18:53 -0800

    Add improve runtime solution to fib question

--- 13. fibonacci.js
+++ 13. fibonacci.js
@@ -6,7 +6,7 @@
 //fib(4)=== 3
 //
 
-//Iterative Solution
+//Iterative Solution: Linear runtime
 function fib(n) {
     const result = [0, 1]
 
@@ -28,3 +28,32 @@ function fib(n) {
 
     return fib(n - 1) + fib(n - 2)
 }
+
+//Solution above is an exponential run time, take a long time to run
+//Question interviewer might ask : Is there to improve the runtime?
+//Answer
+//Memoization: Store the arguments of each fx call along with the result
+//If the function is called again with the same arguments, return the precomputed
+//results, rather than running the fx again
+
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+    };
+}
+
+function slowfib(n) {
+    if (n < 2) {
+        return n
+    }
+
+    return fib(n - 1) + fib(n - 2)
+}
+
+const fib = memoize(slowfib);
