fccf1b7ddb05ac73127c67b5223ec33e3d346343
Dominique Nguyen <domnguyen@gmail.com>
2019-12-19 15:03:52 -0800

    Add stack question

---
+++ 16. stack.js
@@ -0,0 +1,32 @@
+//Create a stack data structure. The stack should be a 
+//class with methonds "push", "pop", and "peek". Adding 
+//an element to the stack should store it until it is
+// removed
+//
+//Example
+//  const s  = new Stack();
+//  s.push(1);
+//  s.push(2);
+//  s.pop() // returns 2
+//  s.pop() // return 1
+
+class Stack {
+    constructor() {
+        this.data = []
+    }
+
+    //Adds data at the end of the array
+    push(record) {
+        this.data.push(record)
+    }
+
+    //Remove data at the end of the array
+    pop() {
+        return this.data.pop();
+    }    
+
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+
+}
